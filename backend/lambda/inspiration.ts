cc1817373553b61d8f815347cca6c522e682f1c1
Marc Mendoza <777marc@gmail.com>
2024-02-09 13:27:04 -0500

    fix secret name

--- backend/lambda/inspiration.ts
+++ backend/lambda/inspiration.ts
@@ -23,7 +23,7 @@ export const handler = async (
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 async function getSecret() {
-  const secret_name = "myTestSecretForLambda";
+  const secret_name = "MyLambdaSecretTest";
 
   const client = new SecretsManagerClient({
     region: "us-east-1",
