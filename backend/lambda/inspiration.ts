7594be4ba42e68e33e4050e9ec6b8c23fba935a0
Marc Mendoza <777marc@gmail.com>
2024-02-09 12:02:40 -0500

    add policy

--- backend/lambda/inspiration.ts
+++ backend/lambda/inspiration.ts
@@ -1,11 +1,48 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from "@aws-sdk/client-secrets-manager";
 
 export const handler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log("", event);
+
+  const secret = getSecret();
+
   return {
-    body: "you are enough",
+    body: `you are enough ${secret}`,
     statusCode: 200,
   };
 };
+
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
+async function getSecret() {
+  const secret_name = "myTestSecretForLambda";
+
+  const client = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+
+  let response;
+
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = response.SecretString;
+
+  return secret;
+}
--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ApiGateway } from "./ApiGateway";
 import { Lambda } from "./Lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,10 @@ export class TodoAppStack extends cdk.Stack {
 
     const healthLambda = new Lambda(this, "health");
     const inspirationLambda = new Lambda(this, "inspiration");
+
+    inspirationLambda.role?.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("SecretsManagerReadWrite")
+    );
 
     api.addIntegration("GET", "/health", healthLambda);
     api.addIntegration("GET", "/inspiration", inspirationLambda);
